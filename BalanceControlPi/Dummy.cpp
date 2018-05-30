481df443e3d085d0998ad52bd0be2becff6690bc
lampeeba16 <marc.lampeebaumgartner@edu.fh-joanneum.at>
2018-05-30 13:06:21 +0200

    seaslukas

---
+++ BalanceControlPi/Dummy.cpp
@@ -0,0 +1,7 @@
+class Dummy
+{ // TODO: delete this class as soon as you have some real code. 
+  // You should first add your code and header files to project BalanceControl, 
+  // then add the existing files to project BalanceLib!
+public:
+	int x;
+};
