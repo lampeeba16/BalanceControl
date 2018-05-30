5d20a998603276cd1ee063f3982d0b8d00ba17da
Michael Faschinger <michivo@gmail.com>
2018-05-30 13:52:09 +0200

    add rocker and ball classes

---
+++ BalanceLib/Rocker.cpp
@@ -0,0 +1,17 @@
+#include "Rocker.h"
+
+Rocker::Rocker(double length, double max_angle, double max_d_angle)
+{
+}
+
+void Rocker::update(std::chrono::duration<double> duration)
+{
+}
+
+void Rocker::reset()
+{
+}
+
+void Rocker::set_target_angle(double target)
+{
+}
---
+++ BalanceLib/Rocker.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <chrono>
+
+class Rocker
+{
+public:
+	Rocker(double length = 2, double max_angle = pi / 8, double max_d_angle = pi / 32);
+
+	void update(std::chrono::duration<double> duration);
+
+	void reset();
+
+	void set_target_angle(double target);
+
+private:
+	static constexpr double pi = 3.14159;
+	double target_angle_;
+	double max_angle_;
+	double max_d_angle_;
+};
