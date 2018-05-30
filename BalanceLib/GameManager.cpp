f2f4a3c0ada3861c066077fafcdc7ed4140ac425
lampeeba16 <marc.lampeebaumgartner@edu.fh-joanneum.at>
2018-05-30 15:13:11 +0200

    PID mit richtigen Werten

--- BalanceLib/GameManager.cpp
+++ BalanceLib/GameManager.cpp
@@ -1,5 +1,7 @@
 #include "GameManager.h"
 #include <math.h>
+#include "PID.h"
+
 GameManager::GameManager()
 {
 }
--- BalanceLib/PID.cpp
+++ BalanceLib/PID.cpp
@@ -4,9 +4,16 @@ PID::PID()
 {
 }
 
-PID::PID(double p_amount, double i_amount, double d_amount, double max_out_pos, double max_out_neg, double control_target, double max_in_pos, double max_in_neg):p_amount_{ p_amount }, i_amount_{i_amount}, d_amount_ {d_amount}, max_out_pos_ {max_in_pos}, max_out_neg_ {max_out_neg}, control_target_{control_target}, max_in_pos_{max_in_pos}, max_in_neg_{max_in_neg}
+PID::PID(double p_amount, double i_amount, double d_amount, double max_out_pos, double max_out_neg, double control_target, double integration_time):
+p_amount_{ p_amount }, 
+i_amount_{i_amount}, 
+d_amount_ {d_amount}, 
+max_out_pos_ {max_out_pos}, 
+max_out_neg_ {max_out_neg}, 
+control_target_{control_target},
+integration_time_{ integration_time}
 {
-
+	pre_error_ = 0;
 }
 
 double PID::Calculate(Ball & Ball)
