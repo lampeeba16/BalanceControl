bd987b3fa992bd5e2898fdd929db88f808251767
Michael Faschinger <michivo@gmail.com>
2018-05-30 14:15:28 +0200

    minor

--- BalanceLib/GameManager.cpp
+++ BalanceLib/GameManager.cpp
@@ -16,12 +16,12 @@ void GameManager::Update(PID &Regler, Ball &Ball, Rocker &Rocker)
 
 void GameManager::Push_Ball(Ball &Ball)
 {
-	Ball.velocity_ = +0.5;
+	Ball.velocity = +0.5;
 }
 
 void GameManager::Reset(Ball &Ball, Rocker &Rocker)
 {
-	Ball.velocity_ = 0;
-	Ball.Position_ = 0; //Oder Center, je nach implementierung
-	Rocker.Reset_angle();
+	Ball.velocity = 0;
+	Ball.position = 0; //Oder Center, je nach implementierung
+	Rocker.reset();
 }
